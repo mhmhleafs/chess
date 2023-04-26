bf8d477b78da9e63d3f18100504cdf23980312b3
Michael Harris <harriswork110@gmail.com>
2023-04-26 10:29:35 -0400

    added piece symbols and streamlined printing

--- boardFunctions.py
+++ boardFunctions.py
@@ -21,7 +21,7 @@ def board_init():
     board = [[],[],[],[],[],[],[],[]]
     for i in range(8):
         for k in range(8):
-            board[i].append(Piece(None, "·"))
+            board[i].append(Piece(None, "e", "."))
     
     return board
 
@@ -30,64 +30,52 @@ def board_print(board, selected_col=None, selected_row=None, movementBoard=None)
 
     temp = "temporary print"
 
-    if(selected_row and selected_col):
+    if selected_col and selected_row:
         colly = numify(selected_col)
 
         print("SELECTED " + colly + "," + str(selected_row))
         selected_col = int(colly)
         selected_row = int(selected_row)
-
-        print("   _______________")
-        for row in range(8):
-            print(row, end="")
-            print(" |", end="")
-            for col in range(8):
-                if(row == selected_row and col == selected_col):
-                    print(selected(board[row][col].type) + "|", end="")
-                    temp = moveset(board, selected_col, selected_row)
-                else:
-                    print(board[row][col].type + "|", end="")
-            print("\n", end="")
-        
-        print("   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
-        print("   a b c d e f g h\n")
-    elif(selected_row and not selected_col):
-        raise Exception("tried selecting piece with row but no col")
-    elif(selected_col and not selected_row):
-        raise Exception("tried selecting piece with col but no now")
     else:
-        print()
-        print("   _______________")
-        for row in range(8):
-            print(row, end="")
-            print(" |", end="")
-            for col in range(8):
-                print(board[row][col].type + "|", end="")
-            print("\n", end="")
-        
-        print("   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
-        print("   a b c d e f g h\n")
+        print("NOTHING SELECTED")
+        selected_col = -1
+        selected_row = -1
+
+    print("   ________________________")
+    for row in range(8):
+        print(row, end="")
+        print(" |", end="")
+        for col in range(8):
+            if(row == selected_row and col == selected_col):
+                print(selected(board[row][col].toString()) + " |", end="")
+                temp = moveset(board, selected_col, selected_row)
+            else:
+                print(board[row][col].toString() + " |", end="")
+        print("\n", end="")
+    
+    print("   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾")
+    print("   a  b  c  d  e  f  g  h\n")
 
     print(temp)
 
 def board_setup(board):    
     for i in range(8):
-        board[1][i] = Piece("black", "P")
+        board[1][i] = Piece("black", "P", "♟︎")
     
-    board[0][0] = board[0][7] = Piece("black", "R")
-    board[0][1] = board[0][6] = Piece("black", "K")
-    board[0][2] = board[0][5] = Piece("black", "B")
-    board[0][3] = Piece("black", "Q")
-    board[0][4] = Piece("black", "G")
+    board[0][0] = board[0][7] = Piece("black", "R", "♜")
+    board[0][1] = board[0][6] = Piece("black", "K", "♞")
+    board[0][2] = board[0][5] = Piece("black", "B", "♝")
+    board[0][3] = Piece("black", "Q", "♛")
+    board[0][4] = Piece("black", "G", "♚")
 
     for i in range(8):
-        board[6][i] = Piece("white", "p")
+        board[6][i] = Piece("white", "p", "♙")
     
-    board[7][0] = board[7][7] = Piece("white", "r")
-    board[7][1] = board[7][6] = Piece("white", "k")
-    board[7][2] = board[7][5] = Piece("white", "b")
-    board[7][3] = Piece("white", "q")
-    board[7][4] = Piece("white", "g")
+    board[7][0] = board[7][7] = Piece("white", "r", "♖")
+    board[7][1] = board[7][6] = Piece("white", "k", "♘")
+    board[7][2] = board[7][5] = Piece("white", "b", "♗")
+    board[7][3] = Piece("white", "q", "♕")
+    board[7][4] = Piece("white", "g", "♔")
 
 def moveset(board, selected_col, selected_row):
     dummyBoard = board_init()
@@ -111,7 +99,6 @@ def moveset(board, selected_col, selected_row):
         return("king at " + str(alphify(selected_col)) + str(selected_row))
     
     return "empy space"
-
 
 def outline_diagonal(board, col, row):
     x = col
@@ -150,7 +137,6 @@ def ooutline_diagonal(board, col, row):
     board_print(dummyBoard)
     print("END")
     return dummyBoard
-
 
 def next_diag(x, y, xOffset, yOffset):
     retX = x + xOffset
--- piece.py
+++ piece.py
@@ -1,4 +1,8 @@
 class Piece:
-    def __init__(self, team, type):
+    def __init__(self, team, type, sym):
         self.team = team
         self.type = type
+        self.sym = sym
+
+    def toString(self):
+        return self.sym
